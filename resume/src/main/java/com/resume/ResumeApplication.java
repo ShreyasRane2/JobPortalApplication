464710c107ea2d9ad80083d9f9f412cb4ec9fc5f
Shreyas Satish Rane <145131378+ShreyasRane2@users.noreply.github.com>
2026-02-25 22:37:48 +0530

    Add files via upload

---
+++ resume/src/main/java/com/resume/ResumeApplication.java
@@ -0,0 +1,17 @@
+package com.resume;
+
+import org.springframework.boot.SpringApplication;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.kafka.annotation.EnableKafka;
+@EnableDiscoveryClient 
+@EnableKafka
+@SpringBootApplication
+public class ResumeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResumeApplication.class, args);
+	}
+
+}
