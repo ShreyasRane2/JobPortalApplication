274c8ad4e197c62b51ada9fb4dadcb9098ea8547
Shreyas Satish Rane <145131378+ShreyasRane2@users.noreply.github.com>
2026-02-25 22:47:05 +0530

    Add files via upload

---
+++ admin-dashboard-microservice/src/main/java/com/admin/microservice/AdminDashboardMicroserviceApplication.java
@@ -0,0 +1,30 @@
+package com.admin.microservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+@EnableDiscoveryClient 
+@SpringBootApplication
+public class AdminDashboardMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdminDashboardMicroserviceApplication.class, args);
+	}
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
